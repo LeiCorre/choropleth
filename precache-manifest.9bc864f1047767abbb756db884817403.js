9c14863869d65d49d013645c79c16d3ec11f73ee
jen.y.loaa@gmail.com <37940404+LeiCorre@users.noreply.github.com>
2020-07-23 18:51:14 -0600

    Updates

---
+++ precache-manifest.9bc864f1047767abbb756db884817403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51061a34abcc61e5abf5a7cbb6066d47",
+    "url": "/choropleth/index.html"
+  },
+  {
+    "revision": "0905a4f078cb599c918c",
+    "url": "/choropleth/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "25fd5c3552f4f35442e5",
+    "url": "/choropleth/static/js/2.95ce5360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/choropleth/static/js/2.95ce5360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0905a4f078cb599c918c",
+    "url": "/choropleth/static/js/main.8d82d080.chunk.js"
+  },
+  {
+    "revision": "4d5437690001743fae1d",
+    "url": "/choropleth/static/js/runtime-main.78ca5cba.js"
+  }
+]);
